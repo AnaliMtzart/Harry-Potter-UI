a1eaf5c903360177b965cd373410567323fb3dd3
AnaliMtzart <analimjack@gmail.com>
2021-06-12 17:34:24 -0500

    feat(home):Add mobile layout static

--- hp-ui/src/App.js
+++ hp-ui/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import HpLogo from './Assets/Images/Harry-Potter-W.png';
+import Favorites from './Assets/Images/favorites.png'
+import Add from './Assets/Images/add.png'
+import './scss/Styles.scss'
+import CardMini from './components/CardMini';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={HpLogo} className='HpLogo' alt='logo'></img>
+      <h3 className='Title'>Selecciona tu filtro</h3>
+      <div className="Btns">
+        <button className='btn'>ESTUDIANTES</button>
+        <button className='btn'>STAFF</button>
+      </div>
+      <div className='Cards'>
+        <CardMini />
+        <CardMini />
+      </div>
+      <div className='GroupBtns'>
+        <button className='Favorites'>FAVORITOS<img src={Favorites} alt='favorites'/></button>
+        <button className='Add'>AGREGAR<img src={Add} alt='add'/></button>
+      </div>
     </div>
   );
 }
