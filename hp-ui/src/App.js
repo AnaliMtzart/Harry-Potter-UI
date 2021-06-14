f7eafd8de8e5a4ac78868cfb3f0b4044e9fd9d93
AnaliMtzart <analimjack@gmail.com>
2021-06-14 03:53:37 -0500

    feat(modal): Add layout

--- hp-ui/src/App.js
+++ hp-ui/src/App.js
@@ -1,5 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React, { useState } from "react";
 import HpLogo from "./Assets/Images/Harry-Potter-W.png";
 import Favorites from "./Assets/Images/favorites.png";
@@ -7,26 +5,28 @@ import Add from "./Assets/Images/add.png";
 import "./scss/Styles.scss";
 import CardMini from "./components/CardMini";
 import data from "./data/hp-characters.json";
+import AddModal from "./components/AddModal";
 
 function App() {
   // console.log(data);
 
-  const [view, setView] = useState('all');
+  const [view, setView] = useState("all");
+  const [modalShow, setModalShow] = useState(false);
 
-  const handleSetStudents = () => setView('students');
-  const handleSetStaff = () => setView('staff');
+  const handleSetStudents = () => setView("students");
+  const handleSetStaff = () => setView("staff");
 
   const students = data.filter((student) => {
     return student.hogwartsStudent === true;
-  })
+  });
 
   // console.log(students)
 
   const staff = data.filter((staff) => {
     return staff.hogwartsStaff === true;
-  })
+  });
 
-   // console.log(staff)
+  // console.log(staff)
 
   return (
     <div className="App">
@@ -40,36 +40,33 @@ function App() {
           STAFF
         </button>
       </div>
-      <div className='Cards'>
-        {/* <CardMini data={data}/>  */}
-        {/* {view ? <CardMini data={students}/> : <CardMini data={staff}/>} */}
-        {/* {viewCard(view)} */}   
-      {(() => {
-        switch (view) {
-          // console.log(view);
-          case "all":   return <CardMini data={data}/>;
-          case "students": return <CardMini data={students}/>;
-          case "staff":  return <CardMini data={staff}/>;
-          default:      return null;
-        }
-      })()}
-    
-
+      <div className="Cards">
+        {(() => {
+          switch (view) {
+            case "all":
+              return <CardMini data={data} />;
+            case "students":
+              return <CardMini data={students} />;
+            case "staff":
+              return <CardMini data={staff} />;
+            default:
+              return null;
+          }
+        })()}
       </div>
       <div className="GroupBtns">
         <button className="Favorites">
           FAVORITOS
           <img src={Favorites} alt="favorites" />
         </button>
-        <button className="Add">
+        <button className="Add" onClick={() => setModalShow(true)}>
           AGREGAR
           <img src={Add} alt="add" />
         </button>
+        <AddModal show={modalShow} onHide={() => setModalShow(false)} />
       </div>
     </div>
   );
 }
-
-
 
 export default App;
